bfc926aaa3b669fb43d4611037dc026592cc7f9b
Robert Bruce <rbruce2@asu.edu>
2018-06-29 16:11:29 -0700

    adding styles

--- src/D8News.js
+++ src/D8News.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { Card, CardImg, CardBody,
-  CardTitle, CardSubtitle, Button, Container, Row, Col } from 'reactstrap';
 import './D8News.css';
 
 class D8News extends Component {
@@ -19,45 +17,53 @@ class D8News extends Component {
     })
   }
 
+  setFirstInterest = interests => {
+    var i = interests;
+    i = i.substring(0, i.indexOf('|'));
+    console.log(i);
+    return i
+  }
+
   setFeedLength = size => {
     if (size === 'Three') {
-      return this.state.ourData.slice(0, 3).map(thisNode => ({ nid: thisNode.node.nid, title: thisNode.node.title, image_url: thisNode.node.image_url, image_alt: thisNode.node.image_alt, path: thisNode.node.path, author: thisNode.node["contributor-contact-information-name"], post_date: thisNode.node.post_date }));
+      return this.state.ourData.slice(0, 3).map(thisNode => ({ nid: thisNode.node.nid, title: thisNode.node.title, image_url: thisNode.node.image_url, image_alt: thisNode.node.image_alt, path: thisNode.node.path, author: thisNode.node["contributor-contact-information-name"], post_date: thisNode.node.post_date, interests: this.setFirstInterest(thisNode.node.interests) }));
     }
     else {
-      return this.state.ourData.map(thisNode => ({ nid: thisNode.node.nid, title: thisNode.node.title, image_url: thisNode.node.image_url, image_alt: thisNode.node.image_alt, path: thisNode.node.path, author: thisNode.node["contributor-contact-information-name"], post_date: thisNode.node.post_date }));
+      console.log(this.state.ourData);
+      return this.state.ourData.map(thisNode => ({ nid: thisNode.node.nid, title: thisNode.node.title, image_url: thisNode.node.image_url, image_alt: thisNode.node.image_alt, path: thisNode.node.path, author: thisNode.node["contributor-contact-information-name"], post_date: thisNode.node.post_date, interests: this.setFirstInterest(thisNode.node.interests) }));
     }
   }
+
 
   render() {
     let results = this.setFeedLength(this.props.dataFromPage.items)
 
     return (
       <div id="D8News">
-        <Container>
-            <Row>
+        <div className="container">
+            <div className="row">
 
               {results.map(( listNode, index ) => {
 
                 return(
+                      <div className="col-12 col-md-4" key={listNode.nid}>
+                        <div className="card">
+                          <img className="card-img-top" src={listNode.image_url} alt={listNode.image_alt} />
+                          <div className="card-body cardBody">
+                            <span className="cardInterest">{listNode.interests}</span>
+                            <h5 className="card-title">{listNode.title}</h5>
+                          </div>
+                        </div>
+                      </div>
 
-                  <Col xs="12" sm="6" md="4" lg="3" xl="3" key={listNode.nid}>
-                    <Card>
-                      <CardImg top width="100%" src={listNode.image_url} alt={listNode.image_alt} />
-                      <CardBody>
-                        <CardTitle>{listNode.title}</CardTitle>
-                        <CardSubtitle>{listNode.author}<br />
-                        {listNode.post_date}</CardSubtitle>
-                        <Button className="btn btn-warning"><a href={listNode.path} className="text-dark" target="_blank">Read More</a></Button>
-                      </CardBody>
-                    </Card>
-                  </Col>
+
 
                 )
 
               })}
 
-            </Row>
-        </Container>
+            </div>
+        </div>
       </div>
     );
   }
