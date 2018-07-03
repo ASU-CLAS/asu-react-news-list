213a729e9ab2d24a463ec180b8e1f5f4247108b6
Robert Bruce <rbruce2@asu.edu>
2018-07-03 16:10:50 -0700

    updated card styles and added saf links

--- src/D8News.js
+++ src/D8News.js
@@ -17,20 +17,57 @@ class D8News extends Component {
     })
   }
 
-  setFirstInterest = interests => {
-    var i = interests;
-    i = i.substring(0, i.indexOf('|'));
-    console.log(i);
-    return i
+  setSafPath = saf => {
+    let path
+    switch (saf) {
+      case 'Global Engagement':
+        path = 'global-engagement'
+        break;
+      case 'Arizona Impact':
+        path = 'arizona-impact'
+        break;
+      case 'Sun Devil Life':
+        path = 'sun-devil-life'
+        break;
+      case 'Creativity':
+        path = 'creativity'
+        break;
+      case 'Discoveries':
+        path = 'discoveries'
+        break;
+      case 'Entrepreneurship':
+        path = 'entrepreneurship'
+        break;
+      case 'Solutions':
+        path = 'solutions'
+        break;
+      default:
+        path = 'asu-news'
+    }
+    return path
   }
 
   setFeedLength = size => {
     if (size === 'Three') {
-      return this.state.ourData.slice(0, 3).map(thisNode => ({ nid: thisNode.node.nid, title: thisNode.node.title, image_url: thisNode.node.image_url, image_alt: thisNode.node.image_alt, path: thisNode.node.path, author: thisNode.node["contributor-contact-information-name"], post_date: thisNode.node.post_date, interests: this.setFirstInterest(thisNode.node.interests) }));
+      return this.state.ourData.slice(0, 3).map(thisNode => ({
+        nid: thisNode.node.nid,
+        title: thisNode.node.title,
+        image_url: thisNode.node.image_url,
+        image_alt: thisNode.node.image_alt,
+        path: thisNode.node.path,
+        saf: thisNode.node.field_saf
+      }));
     }
     else {
       console.log(this.state.ourData);
-      return this.state.ourData.map(thisNode => ({ nid: thisNode.node.nid, title: thisNode.node.title, image_url: thisNode.node.image_url, image_alt: thisNode.node.image_alt, path: thisNode.node.path, author: thisNode.node["contributor-contact-information-name"], post_date: thisNode.node.post_date, interests: this.setFirstInterest(thisNode.node.interests) }));
+      return this.state.ourData.map(thisNode => ({
+        nid: thisNode.node.nid,
+        title: thisNode.node.title,
+        image_url: thisNode.node.image_url,
+        image_alt: thisNode.node.image_alt,
+        path: thisNode.node.path,
+        saf: thisNode.node.field_saf
+      }));
     }
   }
 
@@ -46,12 +83,12 @@ class D8News extends Component {
               {results.map(( listNode, index ) => {
 
                 return(
-                      <div className="col-12 col-md-4 zoom" key={listNode.nid}>
+                      <div className="col-12 col-md-4 mb-4 zoom" key={listNode.nid}>
                         <a href={listNode.path}>
-                          <div className="card">
+                          <div className="card h-100">
                             <img className="card-img-top" src={listNode.image_url} alt={listNode.image_alt} />
-                            <div className="card-body cardBody">
-                              <span className="cardInterest">{listNode.interests ? listNode.interests : 'ASU News'}</span>
+                            <div className={this.setSafPath(listNode.saf) + " card-body cardBody"}>
+                              <a href={listNode.saf ? "//asunow.asu.edu/topics/now/" + this.setSafPath(listNode.saf) : "//asunow.asu.edu/topics/news/saf/asu-news" }><span className={this.setSafPath(listNode.saf) + "Int cardInterest"}>{listNode.saf ? listNode.saf : 'ASU News'}</span></a>
                               <h5 className="card-title">{listNode.title}</h5>
                             </div>
                           </div>
